9268d13e7bed785d3b9291121329cd8846ce6abd
Denys Pravdyvyj <30390172+SO1T@users.noreply.github.com>
2019-03-17 14:07:39 +0200

    Added file upload action

--- client/src/actions/filmAction.js
+++ client/src/actions/filmAction.js
@@ -1,8 +1,8 @@
-import { GET_FILMS, ADD_FILM, DELETE_FILM, FILMS_LOADING} from './types';
+import { GET_FILMS, ADD_FILM, DELETE_FILM, FILMS_LOADING, FILE_UPLOAD} from './types';
 import axios from 'axios';
 
 export const getFilms = () => dispatch => {
-    dispatch(setItemsLoading());
+    dispatch(setFilmsLoading());
     axios
         .get('/api/films')
         .then(res => dispatch({
@@ -11,16 +11,16 @@ export const getFilms = () => dispatch => {
         }))
 };
 
-export const addItem = (item) => dispatch => {
+export const addFilm = (film) => dispatch => {
     axios
-        .post('/api/films/', item)
+        .post('/api/films/', film)
         .then(res => dispatch({
             type: ADD_FILM,
             payload: res.data
-        }))
+        })).catch((err) => alert(err))
 };
 
-export const deleteItem = (id) => dispatch => {
+export const deleteFilm = (id) => dispatch => {
     axios
         .delete(`/api/films/${id}`).then(res => dispatch({
         type: DELETE_FILM,
@@ -28,8 +28,22 @@ export const deleteItem = (id) => dispatch => {
     }))
 };
 
-export const setItemsLoading = () => {
+export const setFilmsLoading = () => {
     return {
         type: FILMS_LOADING
+    }
+};
+
+export const uploadFilms = (file) => dispatch => {
+    alert('sdf')
+    if (file) {
+        const data = new FormData();
+        data.append('file', file, file.name);
+        axios
+            .post('/api/films/upload', data);
+        getFilms();
+    }
+    return {
+        type: FILE_UPLOAD
     }
 };
