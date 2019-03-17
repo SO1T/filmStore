7b0668ff6947e57c9706df7bbad96091027a7cd1
Denys Pravdyvyj <30390172+SO1T@users.noreply.github.com>
2019-03-17 14:08:12 +0200

    Added action type for file upload

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,4 @@ export const GET_FILMS = 'GET_FILMS';
 export const ADD_FILM = 'ADD_FILM';
 export const DELETE_FILM = 'DELETE_FILM';
 export const FILMS_LOADING = 'FILMS_LOADING';
+export const FILE_UPLOAD = 'FILE_UPLOAD';
