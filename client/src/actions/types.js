b80ddd75705ef2f62676cfa746801afd05705dfc
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-24 01:25:47 +0200

    Update types.js

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,5 +3,3 @@ export const ADD_FILM = 'ADD_FILM';
 export const DELETE_FILM = 'DELETE_FILM';
 export const FILMS_LOADING = 'FILMS_LOADING';
 export const FILE_UPLOAD = 'FILE_UPLOAD';
-export const FILM_SORT = 'FILM_SORT';
-export const FIND_FILM_BY = 'FIND_FILM_BY';
