eb596ec7aeb324fd2c226b6019a7cc3d59804c1e
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-24 01:25:44 +0200

    Update films.js

--- routes/api/films.js
+++ routes/api/films.js
@@ -1,34 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const parse = require('../handle/handleFile');
 
-const Film = require('../../models/Film');
+const ctrlFilms = require('../../controllers/films');
 
-router.get('/', (req, res) => {
-    Film.find()
-        .sort({ date: -1 })
-        .then(films => res.json(films))
-});
 
-router.post('/', (req, res) => {
-    const { Title, Release, Format, Stars } = req.body;
-    const newFilm = new Film({
-        Title, Release, Format, Stars
-    });
-    newFilm.save().then(item => res.json(item)).catch(err => res.statusCode(500));
-});
+router.get('/', ctrlFilms.getFilms);
 
-router.delete('/:id', (req, res) => {
-    Film.findById(req.params.id)
-        .then(film => film.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false} ))
-});
+router.post('/', ctrlFilms.addFilm);
 
-router.post('/upload', cors(), (req, res) => {
-    let films = parse(req.files.file.data.toString());
-    // console.log(films);
-    films.forEach(({ Title, Release, Format, Stars }) => new Film({ Title, Release, Format, Stars }).save());
-});
+router.delete('/:id', ctrlFilms.deleteFilm);
+
+router.post('/upload', cors(), ctrlFilms.uploadFile);
 
 module.exports = router;
