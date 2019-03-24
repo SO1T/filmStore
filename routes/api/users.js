f766d46cfcf38ed0d795d088d631597fdd08e07d
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-24 13:50:35 +0200

    Create users.js

---
+++ routes/api/users.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const ctrlFilms = require('../../controllers/users');
+
+const  User = require('../../models/User');
+
+
+router.post('/', (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(404).json({msg: 'Please enter all fields '});
+    }
+
+    User.findOne({ email })
+        .then(user => {
+            if (user) return res.status(400).json({ msj: 'User already exists '});
+            const newUser = new User({ name, email, password });
+
+            // Create salt & hash
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+
+                            jwt.sign(
+                                { id: user.id },
+                                config.get('jwtSecret'),
+                                { expiresIn: 36000 },
+                                (err, token) => {
+                                    if (err) throw err;
+                                    res.json({
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            name: user.name,
+                                            email: user.email
+                                        }
+                                    });
+                                }
+                            )
+                        });
+                });
+            });
+        });
+});
+
+// router.post('/', ctrlFilms.addFilm);
+//
+// router.delete('/:id', ctrlFilms.deleteFilm);
+//
+// router.post('/upload', cors(), ctrlFilms.uploadFile);
+
+module.exports = router;
