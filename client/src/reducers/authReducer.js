ed1a886b1cbfbc02b670dafd9c356aaab55962d0
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-25 22:58:48 +0200

    Update authReducer.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -42,6 +42,7 @@ export default function (state = initialState, { type, payload }) {
         case LOGIN_FAIL:
         case LOGOUT_SUCCESS:
         case REGISTER_FAIL:
+            localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
