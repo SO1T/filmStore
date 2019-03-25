cbe84fdc22a14910e5d26378b2efb9d07df088d2
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-25 21:24:57 +0200

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import filmReducer from './filmReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    film: filmReducer
+    film: filmReducer,
+    auth: authReducer,
+    error: errorReducer
 });
