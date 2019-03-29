c782e18014655e27a8739524eebc6c88b8df0bb5
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-29 17:08:36 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,61 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth');
 
-const ctrlFilms = require('../../controllers/users');
+const ctrlUsers = require('../../controllers/users');
 
 const  User = require('../../models/User');
 
 
-router.post('/', (req, res) => {
-    const { name, email, password } = req.body;
+router.post('/', ctrlUsers.registerUser);
 
-    if (!name || !email || !password) {
-        return res.status(404).json({msg: 'Please enter all fields '});
-    }
-
-    User.findOne({ email })
-        .then(user => {
-            if (user) return res.status(400).json({ msj: 'User already exists '});
-            const newUser = new User({ name, email, password });
-
-            // Create salt & hash
-
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser.save()
-                        .then(user => {
-                            jwt.sign(
-                                { id: user.id },
-                                config.get('jwtSecret'),
-                                { expiresIn: 36000 },
-                                (err, token) => {
-                                    if (err) throw err;
-                                    res.json({
-                                        token,
-                                        user: {
-                                            id: user.id,
-                                            name: user.name,
-                                            email: user.email
-                                        }
-                                    });
-                                }
-                            )
-                        });
-                });
-            });
-        });
-});
-
-router.get('/user', auth, (req, res) => {
-    User.findById(req.user.id)
-        .select('-password')
-        .then(user => res.json(user));
-});
+router.get('/user', auth, ctrlUsers.authUser);
 
 module.exports = router;
