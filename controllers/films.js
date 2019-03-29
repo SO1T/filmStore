1b0a940ab0cb10d9c6fcb58c546a0295ace06bb1
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-29 17:08:04 +0200

    Update films.js

--- controllers/films.js
+++ controllers/films.js
@@ -1,5 +1,4 @@
 const db = require('../models/Film');
-const parse = require('./handle/handleFile');
 
 module.exports.getFilms = function (req, res) {
     db.find()
@@ -18,9 +17,4 @@ module.exports.deleteFilm = function (req, res) {
     db.findById(req.params.id)
         .then(film => film.remove().then(() => res.json({ success: true })))
         .catch(err => res.status(404).json({ success: false }));
-};
-
-module.exports.uploadFile = function (req, res) {
-    let films = parse(req.files.file.data.toString());
-    films.forEach(({ Title, Release, Format, Stars }) => new db({ Title, Release, Format, Stars }).save());
 };
