3e5aaff0cc57e6e6509944999f60a4173f56b29e
Denys Pravdyvyj <dpravdivij@gmail.com>
2019-03-29 17:08:32 +0200

    Update films.js

--- routes/api/films.js
+++ routes/api/films.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const cors = require('cors');
 const auth = require('../../middleware/auth');
 
 const ctrlFilms = require('../../controllers/films');
@@ -11,7 +10,5 @@ router.get('/', ctrlFilms.getFilms);
 router.post('/', auth, ctrlFilms.addFilm);
 
 router.delete('/:id', auth, ctrlFilms.deleteFilm);
-
-router.post('/upload', cors, ctrlFilms.uploadFile);
 
 module.exports = router;
